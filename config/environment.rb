18b94fbad3c2efffb22b00c12543d1ee4df68405
jweiss <jweiss@e1153f85-6c6c-dc11-afa8-0013d3c39b19>
2007-10-17 10:32:34 +0000

    prepare 1.2 release
    git-svn-id: svn+ssh://phoenix/srv/svn/webistrano/trunk@27 e1153f85-6c6c-dc11-afa8-0013d3c39b19

--- config/environment.rb
+++ config/environment.rb
@@ -69,7 +69,7 @@ end
 
 # Include your application configuration below
 
-WEBISTRANO_VERSION = '1.1'
+WEBISTRANO_VERSION = '1.2'
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(:log => '%Y-%m-%d %H:%M')
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(:date_with_day => '%Y-%m-%d')
