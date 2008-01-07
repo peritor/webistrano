ffffb77bfd3721dfb0b48f90093897cb95aa2d6c
jweiss <jweiss@e1153f85-6c6c-dc11-afa8-0013d3c39b19>
2008-01-07 16:39:33 +0000

    update config/ to Rails 2.0.2
    git-svn-id: svn+ssh://phoenix/srv/svn/webistrano/trunk@70 e1153f85-6c6c-dc11-afa8-0013d3c39b19

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
-# Be sure to restart your web server when you modify this file.
+# Be sure to restart your server when you modify this file
 
-# Uncomment below to force Rails into production mode when 
+# Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
@@ -11,13 +11,19 @@ RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
+  # See Rails::Configuration for more options.
 
-  # Skip frameworks you're not going to use (only works if using vendor/rails)
-  # config.frameworks -= [ :active_resource, :action_mailer ]
+  # Skip frameworks you're not going to use (only works if using vendor/rails).
+  # To use Rails without a database, you must remove the Active Record framework
+  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
-  # config.plugins = %W( exception_notification ssl_requirement )
+  # Only load the plugins named here, in the order given. By default, all plugins 
+  # in vendor/plugins are loaded in alphabetical order.
+  # :all can be used as a placeholder for all plugins not explicitly named
+  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
@@ -28,12 +34,15 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
+  # Make sure the secret is at least 30 characters and all random, 
+  # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_test_session',
-    :secret      => 'c856f89ddabee3fc3a14e358a3e9035c'
+    :session_key => '_webistrano_session',
+    :secret      => '4a850072e0e38kvi8#e3abcbf3e0702f0c0cfab35c7e29cbee5e6f267cd8b652ffa24jkasjdh78jk3-0a9:~&@90fb7759e073f0f6ba4b0607bc10324c'
   }
 
-  # Use the database for sessions instead of the file system
+  # Use the database for sessions instead of the cookie-based default,
+  # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
   # config.action_controller.session_store = :active_record_store
 
@@ -47,11 +56,6 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   config.active_record.default_timezone = :utc
-
-  # See Rails::Configuration for more options
-
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory is automatically loaded
 end
 
 # Add new inflection rules using the following format 
