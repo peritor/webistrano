613e542a9232a83c11e2833410d5dae024361d4f
jweiss <jweiss@e1153f85-6c6c-dc11-afa8-0013d3c39b19>
2009-02-25 15:49:15 +0000

    update version number
    git-svn-id: svn+ssh://phoenix/srv/svn/webistrano/trunk@211 e1153f85-6c6c-dc11-afa8-0013d3c39b19

--- config/environment.rb
+++ config/environment.rb
@@ -82,7 +82,7 @@ if WebistranoConfig[:authentication_method] == :cas
   CASClient::Frameworks::Rails::Filter.configure(cas_options[RAILS_ENV])
 end
 
-WEBISTRANO_VERSION = '1.4'
+WEBISTRANO_VERSION = '1.5'
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(:log => '%Y-%m-%d %H:%M')
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(:date_with_day => '%Y-%m-%d')
