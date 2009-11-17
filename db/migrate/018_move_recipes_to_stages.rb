937758fa06702eea573a10307971493cf6ca9cba
Jonathan Weiss <jw@innerewut.de>
2009-11-17 11:02:42 +0100

    fix old migration for Rails 2.3.4

--- db/migrate/018_move_recipes_to_stages.rb
+++ db/migrate/018_move_recipes_to_stages.rb
@@ -1,18 +1,3 @@
-class Recipe < ActiveRecord::Base
-  has_and_belongs_to_many :recipes
-  has_many :stages
-end
-
-class Stage < ActiveRecord::Base
-  has_and_belongs_to_many :recipes
-  belongs_to :project
-end
-
-class Recipe < ActiveRecord::Base
-  has_and_belongs_to_many :projects
-  has_and_belongs_to_many :stages
-end
-
 class MoveRecipesToStages < ActiveRecord::Migration
   def self.up
     
@@ -20,15 +5,7 @@ class MoveRecipesToStages < ActiveRecord::Migration
       t.integer "recipe_id"
       t.integer "stage_id"
     end
-    
-    Recipe.find(:all).each do |r|
-      r.projects.each do |p|
-        p.stages.each do |s|
-          s.recipes << r
-        end
-      end
-    end
-    
+        
   end
 
   def self.down
