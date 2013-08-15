b03b0040f300f861056a88b70572dcb6f8b3decf
Jan Schumann <j.schumann@searchmetrics.com>
2013-08-15 23:39:13 +0200

    correct usage of save without validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,13 +64,13 @@ class User < ActiveRecord::Base
   def remember_me_until(time)
     self.remember_token_expires_at = time
     self.remember_token            = encrypt("#{email}--#{remember_token_expires_at}")
-    save(false)
+    save(:validate => false)
   end
 
   def forget_me
     self.remember_token_expires_at = nil
     self.remember_token            = nil
-    save(false)
+    save(:validate => false)
   end
   
   def admin?
