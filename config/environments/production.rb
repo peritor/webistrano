fc85825e2854dfdf4f41b5142ebe19db4bbfea65
Jan Schumann <j.schumann@searchmetrics.com>
2013-08-16 17:29:31 +0200

    using log level error in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Www::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :error
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
